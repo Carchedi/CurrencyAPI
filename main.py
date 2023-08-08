16581ec8799d780ca62460d366fe98b077afa443
Luiz Carlos Carchedi <carchedi.ufjf@gmail.com>
2023-08-08 13:44:10 -0300

    Checking currency

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import requests
-import json
+import json 
 
 def curr_value(currency):
     req = 'https://api.exchangerate-api.com/v4/latest/'+currency
-    response_API = requests.get(req)
+    response_API = requests.get(req)    
+    
     data = response_API.text
     num = json.loads(data)
     return ("{:.2f}".format(num['rates']['BRL'])) 
 
-print ("Dolar: "+curr_value('USD'))
-print ("Euro: "+curr_value('EUR'))
+dolar = curr_value('USD')
+euro = curr_value('EUR')
+
+print("Dolar: R$ "+dolar)
+print("Euro : R$ "+euro)
  
