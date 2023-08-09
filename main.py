0d8d010b1aca023e3a2b77aade968b0fff9e473d
Luiz Carlos Carchedi <carchedi.ufjf@gmail.com>
2023-08-09 10:36:59 -0300

    codigo atualizado

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import json
 
 def curr_value(currency):
     req = 'https://api.exchangerate-api.com/v4/latest/'+currency
-    response_API = requests.get(req)    
+    response_API = requests.get(req)
     
     data = response_API.text
     num = json.loads(data)
     return ("{:.2f}".format(num['rates']['BRL'])) 
 
 dolar = curr_value('USD')
+
 euro = curr_value('EUR')
 
 print("Dolar: R$ "+dolar)
-print("Euro : R$ "+euro)
- 
+print("Euro : R$ "+euro) 
